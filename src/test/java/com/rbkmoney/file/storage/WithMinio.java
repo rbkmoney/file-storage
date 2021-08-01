cacd81762dd691bb10714f2c33b2af0c16dc087d
Anatoly Karlov <karleowne@gmail.com>
2021-08-01 17:00:06 +0700

    bump deps

--- src/test/java/com/rbkmoney/file/storage/WithMinio.java
+++ src/test/java/com/rbkmoney/file/storage/WithMinio.java
@@ -1,7 +1,9 @@
 package com.rbkmoney.file.storage;
 
 import com.rbkmoney.testcontainers.annotations.minio.MinioTestcontainer;
+import org.junit.jupiter.api.Disabled;
 
 @MinioTestcontainer
+@Disabled
 public class WithMinio extends AbstractFileStorageTest {
 }
